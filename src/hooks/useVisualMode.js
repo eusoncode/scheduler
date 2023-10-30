29a47d8103d69db377302bdf609471bc141592f1
eusoncode <git@github.com:eusoncode/Kata.git>
2023-10-29 21:49:57 -0600

    Adding Transition, Adding Back and Implementing Limits

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,6 +1,23 @@
 import { useState } from "react";
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  return { mode };
+  function transition(newMode, replace = false) {
+    setHistory(prev => (replace
+      ? [...prev.slice(0, prev.length - 1), newMode]
+      : [...prev, newMode]
+    ));
+  }
+
+  function back() {
+    setHistory(prev => {
+      if (prev.length > 1) {
+        return [...prev.slice(0, prev.length - 1)];
+      } else {
+        return [...prev];
+      }
+    });
+  }
+
+  return { mode: history[history.length - 1], transition, back };
 }
